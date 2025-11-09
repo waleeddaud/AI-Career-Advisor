963ef7a76f7ce1a051a3c7277a98e864accca267
Waleed Daud <wdaud727@gmail.com>
2025-11-09 22:28:44 +0500

    Dashboard Details and design

--- CareerAdvisorApp/Controllers/DashboardController.cs
+++ CareerAdvisorApp/Controllers/DashboardController.cs
@@ -2,13 +2,22 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using CareerAdvisorApp.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace CareerAdvisorApp.Controllers;
 public class DashboardController : Controller
-{   
+{
+    private readonly UserManager<IdentityUser> _userManager;
+    public DashboardController(UserManager<IdentityUser> userManager)
+    {
+        _userManager = userManager;
+    }
+    
     [Authorize]
     public IActionResult Index()
     {
-        return View();
+        var user = _userManager.GetUserAsync(User).Result;
+        return View(user);
     }
 }
