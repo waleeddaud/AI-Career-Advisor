ae5741aee1ef25a74864f1affe74927400be7bad
Waleed Daud <wdaud727@gmail.com>
2025-11-24 19:56:31 +0500

    Added migrations for database

---
+++ CareerAdvisorApp/Migrations/20251124144057_CreateCareerDetails.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CareerAdvisorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCareerDetails : Migration
+    {
+        /// <inheritdoc />
+       protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE TABLE CareerDetails (
+                    Id INT IDENTITY(1,1) PRIMARY KEY,
+
+                    UserId NVARCHAR(450) NOT NULL, -- Foreign key to AspNetUsers.Id
+
+                    EducationLevel NVARCHAR(50) NOT NULL,
+                    Skills NVARCHAR(MAX) NOT NULL,
+                    Interests NVARCHAR(MAX) NOT NULL,
+                    CareerGoals NVARCHAR(MAX) NOT NULL,
+                    Experience NVARCHAR(50) NULL,
+                    Industry NVARCHAR(100) NULL,
+                    WorkStyle NVARCHAR(100) NULL,
+                    Salary NVARCHAR(50) NULL,
+                    Timeline NVARCHAR(50) NULL,
+
+                    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+                    UpdatedAt DATETIME NULL,
+
+                    CONSTRAINT FK_CareerDetails_User FOREIGN KEY (UserId)
+                        REFERENCES AspNetUsers(Id) ON DELETE CASCADE
+                )
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TABLE CareerDetails");
+        }
+
+    }
+}
---
+++ CareerAdvisorApp/Migrations/20251124144849_CreateCareerPlan.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CareerAdvisorApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateCareerPlan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                Create table CareerPlan(
+                    Id INT IDENTITY(1,1) PRIMARY KEY,
+                    UserId NVARCHAR(450) NOT NULL,
+                    PlanDetails NVARCHAR(MAX) NOT NULL,
+                    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
+                    UpdatedAt DATETIME NULL,
+                    CONSTRAINT FK_CareerPlan_User FOREIGN KEY (UserId)
+                        REFERENCES AspNetUsers(Id) ON DELETE CASCADE
+                )
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Drop table CareerPlan");
+        }
+    }
+}
